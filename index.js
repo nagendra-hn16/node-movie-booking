a0b692592b7cca002ffda28d5122e22f041f18ae
Nagendra <nhn@apple.com>
2020-10-07 13:43:47 +0530

    new

--- index.js
+++ index.js
@@ -38,9 +38,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: {
-        secure: true,
         key: 'sessionid',
-        httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000
     }
 }))
