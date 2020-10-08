dfc578222a6340d36d0c24a788a2f9f20827cef6
Nagendra <nhn@apple.com>
2020-10-08 13:00:59 +0530

    wtf

--- index.js
+++ index.js
@@ -21,13 +21,15 @@ mongoose.connect(
 
 const loginRoute = require('./routes/login');
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-})
-
 // Enable CORS for all requests
 app.use(cors());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
 
 app.use(bodyParser.json());
 
