f73f66a165cabb4cb2e3cc5f37a86c712849def0
Nagendra <nhn@apple.com>
2020-10-13 18:38:30 +0530

    cookie

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const sessionStore = new MongoStore({
 
 // Enable CORS for all requests
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://peaceful-tor-30705.herokuapp.com',
     credentials: true
 }));
 
@@ -37,7 +37,7 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://peaceful-tor-30705.herokuapp.com');
     res.header('Access-Control-Allow-Methods', 'GET, POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Set-Cookie, Origin, X-Requested-With, Accept');
     next();
